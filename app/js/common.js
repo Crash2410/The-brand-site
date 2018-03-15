fbc49ed2aef9cd4347a76e9f0f6b760ea39419e4
kalnynsh <kda869@yandex.ru>
2018-03-15 16:15:15 +0300

    Update code.

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,5 @@
 // Generate products makup with content
-(function () {
+(function() {
   function generateProducts() {
     var productArray = [
       "Mongo people t-short",
@@ -45,7 +45,8 @@
       var price = Math.floor(min + Math.random() * (max + 1 - min));
       var name = productArray[Math.floor(Math.random() * productArray.length)];
       var rnum = Math.floor(minImg + Math.random() * (maxImg + 1 - minImg));
-      var urlP = `img/item-${rnum}.jpg`;
+      // var urlP = `img/item-${rnum}.jpg`;
+      var urlP = "img/item-" + rnum + ".jpg";
 
       data.products.push({
         id: i,
@@ -85,7 +86,7 @@
   productsGridElem.innerHTML = productsStrMainElem;
 })();
 // Update current Year
-window.onload = function () {
+window.onload = function() {
   var now = new Date();
   var currentYear = now.getFullYear();
   var currentYearElem = document.getElementById("current-year");
@@ -100,22 +101,22 @@ function Cart() {
   this._item = {};
   this._cartId = 0;
 }
-Cart.prototype.getCartId = function () {
+Cart.prototype.getCartId = function() {
   return this._cartId;
 };
 
-Cart.prototype.setNextCartId = function () {
+Cart.prototype.setNextCartId = function() {
   return this._cartId++;
 };
 
-Cart.prototype.resetItem = function () {
+Cart.prototype.resetItem = function() {
   this._item = {};
 };
 
 /**
  * addToStorage - add product to storage
  */
-Cart.prototype.addToStorage = function (item) {
+Cart.prototype.addToStorage = function(item) {
   if (typeof item === "object") {
     this._storage.push(item);
   }
@@ -129,8 +130,8 @@ $(".header-login .cart-sum").hide();
  * корзины, по факту GET AJAX запроса
  * @return {void} Update HTML objects ".cart-amount"
  */
-Cart.prototype.headerCartRender = function () {
-  $.get(CART_URL + "/", function (result) {
+Cart.prototype.headerCartRender = function() {
+  $.get(CART_URL + "/", function(result) {
     if (result) {
       var subSum = 0;
       var totalSum = 0;
@@ -138,7 +139,7 @@ Cart.prototype.headerCartRender = function () {
 
       console.log(result);
 
-      result.forEach(function (item, index) {
+      result.forEach(function(item, index) {
         subSum = parseInt(item.quantity) * parseFloat(item.price);
         totalSum += subSum;
         totalAmount += parseInt(item.quantity);
@@ -159,7 +160,7 @@ var cart = new Cart();
 
 //  обработчики событий
 // нажатие на кнопку добавления в корзину
-$(".product").on("click", function () {
+$(".product").on("click", function() {
   // Получение значений из HTML "item Id", "item Name"
   // присвоение артибутам экзепляра класса cart
   cart._item.itemId = $(this).attr("data-id");
@@ -210,7 +211,7 @@ $(".product").on("click", function () {
 
   // Обработка ввода количества товаров, +/-
   // Handler for 'click' on '.minus-btn'
-  $("body").on("click", ".minus-btn", function (e) {
+  $("body").on("click", ".minus-btn", function(e) {
     e.preventDefault();
     var _num = Number($(".items-to-add-val").val());
     cart._item._currentItemAmount = Number($(".items-to-add-val").val());
@@ -234,7 +235,7 @@ $(".product").on("click", function () {
     //   $(".items-to-add-val").val());
   });
   // Handler for 'click' on '.plus-btn'
-  $("body").on("click", ".plus-btn", function (e) {
+  $("body").on("click", ".plus-btn", function(e) {
     e.preventDefault();
     var _num = Number($(".items-to-add-val").val());
     cart._item._currentItemAmount = Number($(".items-to-add-val").val());
@@ -253,15 +254,16 @@ $(".product").on("click", function () {
       $(".minus-btn").removeClass("disable");
     }
 
-    console.log("After Plus current Item Amount: " +
-      cart._item._currentItemAmount +
-      ' input: ' +
-      $(".items-to-add-val").val());
-
+    console.log(
+      "After Plus current Item Amount: " +
+        cart._item._currentItemAmount +
+        " input: " +
+        $(".items-to-add-val").val()
+    );
   });
 
   // Handler for 'blur' on '.items-to-add-val', check Number of cart._item._currentItemAmount
-  $("body").on("blur", ".items-to-add-val", function () {
+  $("body").on("blur", ".items-to-add-val", function() {
     var num = Number($(".items-to-add-val").val());
     if (num >= 1 && num <= 50) {
       cart._item._currentItemAmount = num;
@@ -283,10 +285,15 @@ $(".product").on("click", function () {
     // console.log("Final current Item Amount after 'blur': " + cart._item._currentItemAmount);
   });
 
-  $("body").on("click", ".fibe-button2", function (e) {
+  $("body").on("click", ".fibe-button2", function(e) {
     e.preventDefault();
 
-    if ($(e).parent().parent().find(".items-to-add-val")) {
+    if (
+      $(e)
+        .parent()
+        .parent()
+        .find(".items-to-add-val")
+    ) {
       cart._item._currentItemAmount = $(".items-to-add-val").val();
       // console.log("After 'Add to cart' final current Item Amount: " + cart._item._currentItemAmount);
       return;
@@ -295,10 +302,15 @@ $(".product").on("click", function () {
     return;
   });
 
-  $("body").on("click", ".fibe-button1", function (e) {
+  $("body").on("click", ".fibe-button1", function(e) {
     e.preventDefault();
 
-    if ($(e).parent().parent().find(".items-to-add-val")) {
+    if (
+      $(e)
+        .parent()
+        .parent()
+        .find(".items-to-add-val")
+    ) {
       cart._item._currentItemAmount = 0;
       // console.log("After 'Cansel' final current Item Amount: " + cart._item._currentItemAmount);
       $(".items-to-add-val").val(1);
@@ -309,23 +321,25 @@ $(".product").on("click", function () {
   });
   // Displaying dialog window passing wrap
   fibe(
-    cart._item.itemName, {
+    cart._item.itemName,
+    {
       text: inputForWindow,
       button1: {
         name: "Cancel"
       },
       button2: {
         name: "Add to cart",
-        action: function () {
+        action: function() {
           $.post(
-            CART_URL + "/", {
+            CART_URL + "/",
+            {
               id: cart.getCartId(),
               itemId: cart._item.itemId,
               itemName: cart._item.itemName,
               price: cart._item.itemPrice,
               quantity: cart._item._currentItemAmount
             }, // Callback after POST
-            function (data) {
+            function(data) {
               cart.addToStorage(cart._item);
               cart.resetItem();
               cart.headerCartRender();
@@ -343,7 +357,7 @@ $(".product").on("click", function () {
                   name: "Continue shopping"
                 },
                 button2: {
-                  action: function () {
+                  action: function() {
                     window.location = "shopping-cart.html";
                   },
                   name: "Make order"
@@ -359,6 +373,6 @@ $(".product").on("click", function () {
 });
 
 // Перенаправление на страницу cart.html
-$(".row-order-btn").on("click", function () {
+$(".row-order-btn").on("click", function() {
   window.location = "shopping-cart.html";
 });
