5961a3639503265e4643085b78d04fb11d62a630
kalnynsh <kda869@yandex.ru>
2018-03-15 09:50:07 +0300

    Made base draft interface for add product to Cart.

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 // Generate products makup with content
 (function() {
   function generateProducts() {
-    const productArray = [
+    var productArray = [
       "Mongo people t-short",
       "Dress",
       "Man's jacket",
@@ -31,21 +31,21 @@
       "Knit Top",
       "Pullover"
     ];
-    let data = {
+    var data = {
       products: []
     };
 
-    const min = 13;
-    const max = 1033;
-    const minImg = 21;
-    const maxImg = 37;
+    var min = 13;
+    var max = 1033;
+    var minImg = 21;
+    var maxImg = 37;
 
     // Create products
-    for (let i = 1; i < 9; i++) {
-      let price = Math.floor(min + Math.random() * (max + 1 - min));
-      let name = productArray[Math.floor(Math.random() * productArray.length)];
-      let rnum = Math.floor(minImg + Math.random() * (maxImg + 1 - minImg));
-      let urlP = `img/item-${rnum}.jpg`;
+    for (var i = 1; i < 9; i++) {
+      var price = Math.floor(min + Math.random() * (max + 1 - min));
+      var name = productArray[Math.floor(Math.random() * productArray.length)];
+      var rnum = Math.floor(minImg + Math.random() * (maxImg + 1 - minImg));
+      var urlP = `img/item-${rnum}.jpg`;
 
       data.products.push({
         id: i,
@@ -57,16 +57,16 @@
     return data;
   }
 
-  let productsGridElem = document.querySelector(".products-grid");
+  var productsGridElem = document.querySelector(".products-grid");
   if (!productsGridElem) console.log("Problem productsGridElem not found");
 
-  let items = generateProducts();
-  let data = items.products;
-  let productStrElem = "";
-  let productsStrMainElem = "";
+  var items = generateProducts();
+  var data = items.products;
+  var productStrElem = "";
+  var productsStrMainElem = "";
 
-  for (let i = 0; i < 8; i++) {
-    for (let key in data[i]) {
+  for (var i = 0; i < 8; i++) {
+    for (var key in data[i]) {
       productStrElem = `
         <div class="product" data-id=${data[i].id}>
             <img src=${
@@ -86,8 +86,227 @@
 })();
 // Update current Year
 window.onload = function() {
-  let now = new Date();
-  let currentYear = now.getFullYear();
-  let currentYearElem = document.getElementById("current-year");
+  var now = new Date();
+  var currentYear = now.getFullYear();
+  var currentYearElem = document.getElementById("current-year");
   currentYearElem.textContent = currentYear;
 };
+// URL for AJAX request handling cart
+var CART_URL = "http://localhost:3800/cart";
+
+// Class Cart
+function Cart() {
+  this._storage = [];
+  this._item = {};
+  this._cartId = 0;
+}
+Cart.prototype.getCartId = function() {
+  return this._cartId;
+};
+
+Cart.prototype.setNextCartId = function() {
+  return this._cartId++;
+};
+
+Cart.prototype.resetItem = function() {
+  this._item = {};
+};
+
+/**
+ * addToStorage - add item to storage
+ */
+Cart.prototype.addToStorage = function(item) {
+  if (typeof item === "object") {
+    this._storage.push(item);
+  }
+  return;
+};
+/**
+ * headerCartRender - отрисовка содержимого
+ * корзины, по факту  запроса
+ * @return {[type]} [description]
+ */
+Cart.prototype.headerCartRender = function() {
+  $.get(CART_URL + "/", function(result) {
+    if (result) {
+      var subSum = 0;
+      var totalSum = 0;
+      var totalAmount = 0;
+
+      console.log(result);
+
+      result.forEach(function(item, index) {
+        subSum = parseInt(item.quantity) * parseFloat(item.price);
+        totalSum += subSum;
+        totalAmount += parseInt(item.quantity);
+      });
+      $(".cart-amount").text("Total amount: " + totalAmount);
+      $(".cart-sum").text("Total sum: $" + totalSum);
+    } else {
+      console.log("Server not response");
+    }
+  });
+};
+
+let cart = new Cart();
+
+//  обработчики событий
+// нажатие на кнопку добавления в корзину
+$(".product").on("click", function() {
+  // Получение значений из HTML "item Id", "item Name"
+  // присвоение артибутам экзепляра класса cart
+  cart._item.itemId = $(this).attr("data-id");
+  cart._item.itemName = $(this)
+    .find(".product__title")
+    .text()
+    .trim();
+  cart._item.itemPrice = $(this)
+    .find(".product__price_accent")
+    .text()
+    .trim()
+    .slice(1);
+  cart.setNextCartId();
+
+  console.log(cart._item);
+
+  // Генерация модульного окна
+  // Generate wrap with -/+ input element for dialog window
+  var inputForWindow = $("<div>");
+  var subtitle = $("<div>")
+    .addClass("fibe-subtitle")
+    .text("How much do you want?");
+  var form = $("<div>").addClass("add-form");
+  var minusBtn = $("<div>").addClass("minus-btn disable");
+  var minus = $("<i>").addClass("fas fa-minus");
+  var plusBtn = $("<div>").addClass("plus-btn");
+  var plus = $("<i>").addClass("fas fa-plus");
+  var input = $("<input>")
+    .addClass("items-to-add-val")
+    .attr({
+      name: "quantity",
+      type: "number",
+      min: 1,
+      max: 50,
+      step: 1,
+      value: 1
+    });
+
+  minusBtn.append(minus);
+  plusBtn.append(plus);
+  form
+    .append(minusBtn)
+    .append(input)
+    .append(plusBtn);
+  inputForWindow.append(subtitle).append(form);
+  // END of generate wrap
+
+  // Displaying dialog window passing wrap
+  fibe(
+    cart._item.itemName,
+    {
+      text: inputForWindow,
+      button1: { name: "Cancel" },
+      button2: {
+        name: "Add to cart",
+        action: function() {
+          $.post(
+            CART_URL + "/",
+            {
+              id: cart.getCartId(),
+              itemId: cart._item.itemId,
+              itemName: cart._item.itemName,
+              price: cart._item.itemPrice,
+              quantity: cart._item._currentItemAmount
+                ? cart._item._currentItemAmoun
+                : 1
+            }, // Callback after POST
+            function(data) {
+              cart.addToStorage(cart._item);
+              cart.resetItem();
+              cart.headerCartRender();
+
+              var addedName = data.itemName;
+              var message = $("<span>").text("Added! ");
+              var wrap = $("<div>")
+                .append(message)
+                .append(data.quantity + " piece`s in cart.");
+
+              // Displaying 'success' dialog window
+              fibe(addedName, "success", false, {
+                text: wrap,
+                button1: {
+                  name: "Continue shopping"
+                },
+                button2: {
+                  action: function() {
+                    window.location = "shopping-cart.html";
+                  },
+                  name: "Make order"
+                }
+              });
+            }
+          );
+        }
+      }
+    },
+    false
+  );
+});
+
+// Обработка ввода количества товаров, +/-
+// Handler for 'click' on '.minus-btn'
+$("body").on("click", ".minus-btn", function() {
+  cart._item._currentItemAmount = Number($(".items-to-add-val").val());
+  if (cart._item._currentItemAmount <= 1) {
+    return;
+  }
+  $(".items-to-add-val").val(cart._item._currentItemAmount - 1);
+  cart._item._currentItemAmount--;
+  if (cart._item._currentItemAmount <= 1) {
+    $(".minus-btn").addClass("disable");
+  }
+  if (cart._item._currentItemAmount < 50) {
+    $(".plus-btn").removeClass("disable");
+  }
+});
+// Handler for 'click' on '.plus-btn'
+$("body").on("click", ".plus-btn", function() {
+  cart._item._currentItemAmount = Number($(".items-to-add-val").val());
+  if (cart._item._currentItemAmount >= 50) {
+    return;
+  }
+  $(".items-to-add-val").val(cart._item._currentItemAmount + 1);
+  cart._item._currentItemAmount++;
+  if (cart._item._currentItemAmount >= 50) {
+    $(".plus-btn").addClass("disable");
+  }
+  if (cart._item._currentItemAmount > 1) {
+    $(".minus-btn").removeClass("disable");
+  }
+});
+// Handler for 'blur' on '.items-to-add-val', check Number of cart._item._currentItemAmount
+$("body").on("blur", ".items-to-add-val", function() {
+  var num = Number($(".items-to-add-val").val());
+  if (num >= 1 && num <= 50) {
+    cart._item._currentItemAmount = num;
+    if (cart._item._currentItemAmount >= 50) {
+      $(".plus-btn").addClass("disable");
+    }
+    if (cart._item._currentItemAmount > 1) {
+      $(".minus-btn").removeClass("disable");
+    }
+    if (cart._item._currentItemAmount <= 1) {
+      $(".minus-btn").addClass("disable");
+    }
+    if (cart._item._currentItemAmount < 50) {
+      $(".plus-btn").removeClass("disable");
+    }
+  } else {
+    $(".items-to-add-val").val(1);
+  }
+});
+
+// Перенаправление на страницу cart.html
+$(".row-order-btn").on("click", function() {
+  window.location = "shopping-cart.html";
+});
